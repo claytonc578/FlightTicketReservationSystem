597c41c266742bd3b229bbe2f56e9bd654f3f5d6
Clayton Cheng <claytoncheng@Claytons-MBP.hsd1.ca.comcast.net>
2020-10-18 16:56:41 -0700

    Adding django project

---
+++ flight_reservation/reservation/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Passenger, Plane, Flight
+
+def index(request):
+    return HttpResponse("Hello world. You are at the reservation index")
+
+def home(request):
+    context = {
+        'flights': Flight.objects.all()
+    }
+    return render(request, 'reservation/home.html', context)
